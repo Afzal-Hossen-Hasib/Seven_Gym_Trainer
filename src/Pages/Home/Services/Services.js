fb8a16c2c7c3ac79a8cebe9b04e0c90136341e3e
afzalhossenhasib <ahhasib47@gmail.com>
2022-04-16 23:43:53 +0600

    fake data added

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Services = () => {
+
+    const [] = useState();
+
+    return (
+        <div>
+            <h1>This Is Services:</h1>
+        </div>
+    );
+};
+
+export default Services;
