aaa4d5045843b46b8eb42e8e047fe996e5c1a33f
afzalhossenhasib <ahhasib47@gmail.com>
2022-04-17 21:06:16 +0600

    readme file edit

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -4,7 +4,7 @@ import './Service.css'
 
 const Service = ({service}) => {
 
-    const {name, picture, desription, id} = service;
+    const {name, picture, price, desription, id} = service;
     const navigate = useNavigate();
 
     const navigteToServiceSection = id => {
@@ -16,6 +16,7 @@ const Service = ({service}) => {
             <img src={picture} alt="" />
             <h4 className='my-4'>{name}</h4>
             <p>{desription}</p>
+            <p className='text-center'><span className='fw-bold'>Price:</span> ${price}</p>
             <button className='service-button' onClick={() => navigteToServiceSection(id)}>Check Out</button>
         </div>
     );
